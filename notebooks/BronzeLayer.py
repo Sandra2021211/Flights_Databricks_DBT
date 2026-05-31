526c0d2e332849b3a2ce7fb5c10540eb9d8a872a
Sandra2021211 <87116316+Sandra2021211@users.noreply.github.com>
2026-05-31 15:26:34 +0000

    added code for data ingestion from rawvolume to bronzevolume

---
+++ notebooks/BronzeLayer.py
@@ -0,0 +1,63 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### **INCREMENTAL DATA INGESTION**
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE VOLUME workspace.bronze.bronzevolume
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE VOLUME workspace.silver.silvervolume
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE VOLUME workspace.gold.goldvolume
+
+# COMMAND ----------
+
+dbutils.widgets.text("src","")
+
+# COMMAND ----------
+
+src_value = dbutils.widgets.get("src")
+
+# COMMAND ----------
+
+df = spark.readStream.format("cloudFiles")\
+          .option("cloudFiles.format", "csv")\
+          .option("cloudFiles.schemaLocation",f"/Volumes/workspace/bronze/bronzevolume/{src_value}/checkpoint")\
+          .option("cloudFiles.schemaEvolutionMode", "rescue")\
+          .load(f"/Volumes/workspace/raw/rawvolume/rawdata/{src_value}/")
+
+# COMMAND ----------
+
+df.writeStream.format("delta")\
+              .outputMode("append")\
+              .trigger(once=True)\
+              .option("checkpointLocation",f"/Volumes/workspace/bronze/bronzevolume/{src_value}/checkpoint")\
+              .option("path",f"/Volumes/workspace/bronze/bronzevolume/{src_value}/data")\
+              .start()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM delta. `/Volumes/workspace/bronze/bronzevolume/bookings/data`
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM delta. `/Volumes/workspace/bronze/bronzevolume/flights/data`
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM delta. `/Volumes/workspace/bronze/bronzevolume/customers/data`
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM delta. `/Volumes/workspace/bronze/bronzevolume/airports/data`
